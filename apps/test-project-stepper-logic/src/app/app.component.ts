59b8e7c01da8b5ead39f659b97db0d6689a24992
Charlie Greenman <charlie@razroo.com>
2023-11-14 18:08:07 -0500

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/test-project-stepper-logic/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-project-stepper-logic-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-project-stepper-logic';
+}
